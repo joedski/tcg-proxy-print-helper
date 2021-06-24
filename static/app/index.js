92331f77a622080f91e7409b0b3f98bf80d6439e
Joseph Sikorski <joedski@gmail.com>
2021-06-23 21:33:15 -0400

    Starting new uhtml based app, setup card list form

--- static/app/index.js
+++ static/app/index.js
@@ -1,154 +1,87 @@
 // @ts-check
+import { render, html } from "./uhtml.js";
 
-import * as R from "./replacer.js";
-import * as E from "./events.js";
+app(document.querySelector(".tcg-card-set"), CardProxiesApp);
 
-const cardProxyEls = [...document.querySelectorAll(".mtg-proxy-card")];
-const cardJsonLink = document.querySelector('link[rel="mtg:cardlist"]');
-const cardJsonHref = cardJsonLink.getAttribute("href");
+function app(where, component) {
+  const ctx = {
+    state: initialState(),
+    update(mapState) {
+      ctx.state = mapState(ctx.state);
+      render(where, component(ctx));
+    },
+  };
 
-if (!cardJsonHref) {
-  throw new Error(`Invalid Card JSON Href: ${cardJsonHref}`);
+  return render(where, component(ctx));
 }
 
-const cardJsonPromise = fetch(cardJsonHref)
-  .then((response) => {
-    if (response.ok) return response.json();
-    throw Object.assign(
-      new Error(
-        `Error loading Card JSON: ${response.status} ${response.statusText}`
-      ),
-      { response }
-    );
-  })
-  .then((cardJson) => {
-    cardDb = cardJson;
-    return cardJson;
-  });
-
-let cardDb = [];
-
-cardProxyEls.forEach(cardProxyHelper);
-
-function cardProxyHelper(rootEl) {
-  rootEl.$state = {
+function initialState() {
+  return {
     form: {
-      cardNameSearch: "",
+      cardListText: "",
     },
     currentCard: null,
   };
-
-  E.listen(rootEl, {
-    "click .tcg-proxy__search-result-item": (event) => {
-      const scryfallId = event.target.dataset.scryfallId;
-
-      if (!scryfallId) return;
-
-      const nextCard = getCardById(scryfallId);
-
-      update((state) => ({
-        ...state,
-        currentCard: nextCard,
-      }));
-    },
-    "keyup .tcg-proxy__name-search": (event) => {
-      update((state) => ({
-        ...state,
-        form: {
-          ...state.form,
-          cardNameSearch: event.target.value,
-        },
-      }));
-    },
-  });
-
-  // Yeah, I should really just use µhtml.
-  R.withElementAndRoot(
-    rootEl,
-    document.body,
-    R.template("#proxy-card-template", {})
-  );
-
-  update((s) => s);
-
-  cardJsonPromise.then(() => update((s) => s));
-
-  function update(mapState) {
-    rootEl.$state = mapState(rootEl.$state);
-    draw(rootEl.$state);
-  }
-
-  function draw(state) {
-    R.withElementAndRoot(
-      rootEl,
-      document.body,
-      R.replaceIn({
-        ".tcg-proxy__name-search": R.updateEl((el) => {
-          el.value = state.form.cardNameSearch;
-        }),
-        ".tcg-proxy__search-results":
-          state.form.cardNameSearch.length >= 3
-            ? searchCardDb(state.form.cardNameSearch)
-                .slice(0, 10)
-                .map((cardObject) =>
-                  R.compose(
-                    R.updateEl((fragment) => {
-                      fragment.children[0].dataset.scryfallId = cardObject.id;
-                      return fragment;
-                    }),
-                    R.template("#proxy-card-search-result-item-template", {
-                      ".tcg-proxy__search-result-item__name": R.text(
-                        cardObject.name
-                      ),
-                      ".tcg-proxy__search-result-item__misc": R.text(
-                        renderCardMisc(cardObject)
-                      ),
-                    })
-                  )
-                )
-            : [],
-        ".tcg-proxy__card": R.style({
-          backgroundImage: state.currentCard
-            ? `url(${state.currentCard.image_uris.normal})`
-            : "",
-        }),
-      })
-    );
-  }
 }
 
-function renderCardMisc(cardObject) {
-  const parts = [cardObject.set, `#${cardObject.collector_number}`];
+function CardProxiesApp(ctx) {
+  return html` ${CardProxiesForm(ctx)} ${CardProxyGrid(ctx)} `;
+}
 
-  if (cardObject.foil) {
-    parts.push("Foil");
+function CardProxiesForm(ctx) {
+  return html`
+    <div class="tcg-proxy__form">
+      <h1>MTG Proxies Helper</h1>
+      <p>
+        Enter a TCGPlayer-style list of cards below in "(Count) (Cardname)
+        [Set]" format.
+      </p>
+      <textarea
+        class="tcg-proxy__card-list"
+        type="text"
+        placeholder="4 Black Lotus
+4 Ancestral Recall [2ed]"
+        onkeyup=${onCardListKeyup(ctx)}
+      >
+${ctx.state.form.cardListText}</textarea
+      >
+    </div>
+  `;
+}
+
+function onCardListKeyup(ctx) {
+  return (event) => {
+    ctx.update((state) => ({
+      ...state,
+      form: {
+        ...state.form,
+        cardListText: event.target.value,
+      },
+    }));
+  };
+}
+
+function CardProxyGrid(ctx) {
+  return html`
+    <div class="tcg-card-row">
+      <div class="tcg-card">
+        Test:
+        <pre>${ctx.state.form.cardListText}</pre>
+      </div>
+    </div>
+  `;
+}
+
+function partition(size) {
+  function $partition(parts, next) {
+    if (!parts.length || parts[parts.length - 1].length >= size) {
+      parts.push([]);
+    }
+
+    const last = parts[parts.length - 1];
+
+    last.push(next);
+
+    return parts;
   }
-
-  return parts.join(" ");
-}
-
-function searchCardDb(text) {
-  return cardDb
-    .filter(
-      (card) => card.name.toLowerCase().indexOf(text.toLowerCase()) !== -1
-    )
-    .sort((a, b) => {
-      const nameResult = a.name.localeCompare(b.name);
-      if (nameResult !== 0) return nameResult;
-
-      const dateResult = -1 * a.released_at.localeCompare(b.released_at);
-      if (dateResult !== 0) return dateResult;
-
-      const setResult = a.set.localeCompare(b.released_at);
-      if (setResult !== 0) return setResult;
-
-      const foilResult = (a.foil ? "B" : "A").localeCompare(b.foil ? "B" : "A");
-      if (foilResult !== 0) return foilResult;
-
-      return 0;
-    });
-}
-
-function getCardById(scryfallId) {
-  return cardDb.find((card) => card.id === scryfallId);
 }
