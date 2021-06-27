adc88c81ba33d3c276e03edc6171f3242a9d5199
Joseph Sikorski <joedski@gmail.com>
2021-06-27 17:19:03 -0400

    AsyncData type
    Man, look at all that copy/pasta.

--- static/app/util.js
+++ static/app/util.js
@@ -2,8 +2,9 @@
 
 /**
  * Utility predicate to make case-based logic easier.
- * @template {Function} C
- * @param {[Function, ...any[]]} thisCase
+ * @template {String} TAll
+ * @template {TAll} C
+ * @param {[TAll, ...any[]]} thisCase
  * @param {C} desired
  * @returns {thisCase is [C, ...any[]]}
  */
@@ -22,7 +23,7 @@ export class Result {
    * @returns {Result<T, any>}
    */
   static Ok(value) {
-    return new Result([Result.Ok, value]);
+    return new Result(["Ok", value]);
   }
 
   /**
@@ -31,7 +32,7 @@ export class Result {
    * @returns {Result<any, E>}
    */
   static Failure(value) {
-    return new Result([Result.Failure, value]);
+    return new Result(["Failure", value]);
   }
 
   /**
@@ -65,12 +66,12 @@ export class Result {
   }
 
   constructor(
-    /** @type {[typeof Result.Ok, T] | [typeof Result.Failure, E]} */
+    /** @type {['Ok', T] | ['Failure', E]} */
     thisCase
   ) {
     switch (thisCase[0]) {
-      case Result.Ok:
-      case Result.Failure:
+      case "Ok":
+      case "Failure":
         break;
 
       default:
@@ -96,7 +97,7 @@ export class Result {
   map(fn) {
     const thisCase = this.case;
 
-    if (taggedSumCaseIs(thisCase, Result.Ok)) {
+    if (taggedSumCaseIs(thisCase, "Ok")) {
       return Result.Ok(fn(thisCase[1]));
     }
 
@@ -123,13 +124,149 @@ export class Result {
    */
   join() {
     switch (this.case[0]) {
-      case Result.Ok:
+      case "Ok":
         if (this.case[1] instanceof Result) return this.case[1];
         throw new Error(`Cannot join non-Result value ${this.case[1]}`);
 
       default:
-      case Result.Failure:
+      case "Failure":
         return /** @type {Result<any, E>} */ (this);
     }
   }
 }
+
+/**
+ * @template T
+ * @template E
+ */
+export class AsyncData {
+  static NotAsked() {
+    return $notAsked;
+  }
+
+  static Waiting() {
+    return $waiting;
+  }
+
+  /**
+   * @template T
+   * @param {T} value
+   * @returns {AsyncData<T, any>}
+   */
+  static Data(value) {
+    return new AsyncData(["Data", value]);
+  }
+
+  /**
+   * @template E
+   * @param {E} value
+   * @returns {AsyncData<any, E>}
+   */
+  static Error(value) {
+    return new AsyncData(["Error", value]);
+  }
+
+  /**
+   * @template T
+   * @param {() => T} fn
+   * @returns {AsyncData<T, any>}
+   */
+  static ofCall(fn) {
+    try {
+      return AsyncData.Data(fn());
+    } catch (error) {
+      return AsyncData.Error(error);
+    }
+  }
+
+  /**
+   * Transforms an ordinary function that may throw into a function
+   * returning a AsyncData.
+   *
+   * Note that because you can throw _anything_ in JS, the failure type
+   * is `unknown`.  It could even be `undefined`!
+   * @template R
+   * @template {any[]} TArgs
+   * @param {(...args: TArgs) => R} fn
+   * @returns {(...args: TArgs) => AsyncData<R, unknown>}
+   */
+  static liftThrowing(fn) {
+    return function $lifted(...args) {
+      return AsyncData.ofCall(() => fn.apply(this, args));
+    };
+  }
+
+  constructor(
+    /** @type {['NotAsked'] | ['Waiting'] | ['Data', T] | ['Error', E]} */
+    thisCase
+  ) {
+    switch (thisCase[0]) {
+      case "NotAsked":
+      case "Waiting":
+      case "Data":
+      case "Error":
+        break;
+
+      default:
+        throw new Error(`Unrecognized tag ${thisCase[0]}`);
+    }
+
+    this.case = thisCase;
+  }
+
+  toJson() {
+    return {
+      type: "AsyncData",
+      tag: this.case[0],
+      value: this.case[1],
+    };
+  }
+
+  /**
+   * @template U
+   * @param {(value: T) => U} fn
+   * @returns {AsyncData<U, E>}
+   */
+  map(fn) {
+    const thisCase = this.case;
+
+    if (taggedSumCaseIs(thisCase, "Data")) {
+      return AsyncData.Data(fn(thisCase[1]));
+    }
+
+    return /** @type {AsyncData<any, E>} */ (this);
+  }
+
+  /**
+   *
+   * @template U
+   * @template F
+   * @param {(value: T) => AsyncData<U, F>} fn
+   * @returns {AsyncData<U, E | F>}
+   */
+  flatMap(fn) {
+    return this.map(fn).join();
+  }
+
+  // Not really sure how to get this one working type-wise in jsdocs.
+  /**
+   * @template TInner
+   * @template EInner
+   * @this {AsyncData<AsyncData<TInner, EInner>, E>}
+   * @returns {AsyncData<TInner, EInner | E>}
+   */
+  join() {
+    switch (this.case[0]) {
+      case "Data":
+        if (this.case[1] instanceof AsyncData) return this.case[1];
+        throw new Error(`Cannot join non-AsyncData value ${this.case[1]}`);
+
+      default:
+      case "Error":
+        return /** @type {AsyncData<any, E>} */ (this);
+    }
+  }
+}
+
+const $notAsked = new AsyncData(["NotAsked"]);
+const $waiting = new AsyncData(["Waiting"]);
